429fcb41e42c71950e013c7d1a2f85babb5432f2
DESKTOP-DLOBL4I\张文喆 <942740938@qq.com>
2017-11-01 15:59:59 +0800

    Q9实现AVL树

--- Q9/orderTree.h
+++ Q9/orderTree.h
@@ -5,10 +5,11 @@ using namespace std;
 class Node {
 public:
 	Node(int num)
-		:num(num), left(NULL), right(NULL) {}
+		:num(num), left(NULL), right(NULL), height(0) {}
 	int num;
 	Node* left;
 	Node* right;
+	int height;
 };
 
 class OrderTree {
@@ -18,7 +19,14 @@ public:
 	~OrderTree();
 	Node* getRoot() { return root; }
 	void releaseNode(Node*);
-	bool insert(const int);
+	bool insert(const int);//插入普通二叉查找树
+	Node* insert(const int, Node*);//插入AVL树
+	int getHeight(Node*);//获得节点高度，空节点高度为-1
+	int max(const int, const int);
+	Node* singleRotateWithLeft(Node*);//左子树单旋转
+	Node* singleRotateWithRight(Node*);//右子树单旋转
+	Node* doubleRotateWithLeft(Node*);//左子树双旋转
+	Node* doubleRotateWithRight(Node*);//右子树双旋转
 	bool searh(const int);
 	void showAll(Node*);
 	bool isEmpty;
@@ -85,6 +93,105 @@ bool OrderTree::insert(const int num)
 	root = newNode;//到达此处则说明root为NULL
 	isEmpty = false;//二叉排序树不再为空
 	return true;
+}
+
+Node* OrderTree::insert(const int num, Node* tree)
+{
+	if (tree == NULL)//将新节点插在空位置上
+	{
+		tree = new Node(num);
+		if (tree == NULL)
+		{
+			cerr << "空间不足，插入失败！" << endl;
+			return NULL;
+		}
+	}
+	else if(num < tree->num)//插在左子树中
+	{
+		tree->left = insert(num, tree->left);
+		if (getHeight(tree->left) - getHeight(tree->right) == 2)//破坏了AVL特性
+		{
+			/*新节点插入到了左儿子的左子树中，要求进行左子树单旋转*/
+			if (num < tree->left->num)
+				tree = singleRotateWithLeft(tree);
+			/*新节点插入到了左儿子的右子树中，要求进行左子树双旋转*/
+			else
+				tree = doubleRotateWithLeft(tree);
+		}
+	}
+	else if (num > tree->num)//插在右子树中
+	{
+		tree->right = insert(num, tree->right);
+		if (getHeight(tree->right) - getHeight(tree->left) == 2)//破坏了AVL特性
+		{
+			/*新节点插入到了右儿子的右子树中，要求进行右子树单旋转*/
+			if (num > tree->right->num)
+				tree = singleRotateWithRight(tree);
+			/*新节点插入到了右儿子的左子树中，要求进行右子树双旋转*/
+			else
+				tree = doubleRotateWithRight(tree);
+		}
+	}
+	else//num已存在于AVL树中
+	{
+		cerr << "The input key(" << num << ") has been in!" << endl;
+		return tree;
+	}
+
+	tree->height = max(getHeight(tree->left), getHeight(tree->right));//更新根的高度
+	return tree;
+}
+
+int OrderTree::getHeight(Node* node)
+{
+	if (node == NULL)//空节点高度为-1
+		return -1;
+	else
+		return node->height;
+}
+
+int OrderTree::max(const int num1, const int num2)
+{
+	if (num1 > num2)
+		return num1;
+	else
+		return num2;
+}
+
+Node* OrderTree::singleRotateWithLeft(Node* tree)
+{
+	Node* newTree = tree->left;
+	tree->left = newTree->right;
+	newTree->right = tree;
+
+	tree->height = max(getHeight(tree->left), getHeight(tree->right)) + 1;
+	newTree->height = max(getHeight(newTree->left), getHeight(newTree->right)) + 1;
+
+	return newTree;
+}
+
+Node* OrderTree::singleRotateWithRight(Node* tree)
+{
+	Node* newTree = tree->right;
+	tree->right = newTree->left;
+	newTree->left = tree;
+
+	tree->height = max(getHeight(tree->left), getHeight(tree->right)) + 1;
+	newTree->height = max(getHeight(newTree->left), getHeight(newTree->right)) + 1;
+
+	return newTree;
+}
+
+Node* OrderTree::doubleRotateWithLeft(Node* tree)
+{
+	tree->left = singleRotateWithRight(tree->left);
+	return singleRotateWithLeft(tree);
+}
+
+Node* OrderTree::doubleRotateWithRight(Node* tree)
+{
+	tree->right = singleRotateWithLeft(tree->right);
+	return singleRotateWithRight(tree);
 }
 
 bool OrderTree::searh(const int num)
